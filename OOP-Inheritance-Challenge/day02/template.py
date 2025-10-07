71479d1ea774b622dee001ee96ccd08c67843cc3
khrun_nizam <mohammadkhoirunnizam69@gmail.com>
2025-10-07 10:08:52 +0700

    update_template.py

--- OOP-Inheritance-Challenge/day02/template.py
+++ OOP-Inheritance-Challenge/day02/template.py
@@ -1,11 +1,22 @@
 class Kendaraan:
-    pass
+    def __init__(self, merk, kecepatan_maksimal):
+        self.merk = merk
+        self.kecepatan_maksimal = kecepatan_maksimal
 
+    def berjalan(self):
+        return f"{self.merk} sedang berjalan dengan kecepatan {self.kecepatan_maksimal} km/jam."
+        
 class Mobil(Kendaraan):
-    pass
+    def berjalan(self):
+        return f"Mobil {self.merk} melaju dengan kecepatan maksimal {self.kecepatan_maksimal} km/jam."
 
 class Motor(Kendaraan):
-    pass
+    def berjalan(self):
+        return f"Motor {self.merk} meluncur dengan kecepatan maksimal {self.kecepatan_maksimal} km/jam."
 
 if __name__ == "__main__":
-    pass
+    mobil1 = Mobil("Porsche 911 GT2 RS", 340)
+    motor1 = Motor("Kawasaki Ninja ZX-14R", 299)
+
+    print(mobil1.berjalan())
+    print(motor1.berjalan())
