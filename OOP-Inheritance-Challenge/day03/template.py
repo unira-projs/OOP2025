6f940357415996fcf707497a615a670cadd9eb39
khrun_nizam <mohammadkhoirunnizam69@gmail.com>
2025-10-07 09:54:08 +0700

    Update template.py

--- OOP-Inheritance-Challenge/day03/template.py
+++ OOP-Inheritance-Challenge/day03/template.py
@@ -1,8 +1,22 @@
 class Pegawai:
-    pass
+    def __init__(self, nama, gaji_dasar):
+        self.nama = nama
+        self.gaji_dasar = gaji_dasar
+
+    def hitung_gaji(self):
+        return self.gaji_dasar
 
 class Manager(Pegawai):
-    pass
+    def __init__(self, nama, gaji_dasar, tunjangan):
+        super().__init__(nama, gaji_dasar)
+        self.tunjangan = tunjangan
+        
+    def hitung_gaji(self):
+        return super().hitung_gaji() + self.tunjangan    
 
 if __name__ == "__main__":
-    pass
+    pegawai1 = Pegawai("Asep", 5000000)
+    manager1 = Manager("Nizam", 7000000, 3000000)
+
+    print(f"Gaji {pegawai1.nama}: Rp{pegawai1.hitung_gaji()}")
+    print(f"Gaji {manager1.nama}: Rp{manager1.hitung_gaji()}")
